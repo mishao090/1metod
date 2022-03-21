4a085cf07457fd028b51ba5739b73f34eb46956f
mishao090 <michailus321@gmail.com>
2022-03-21 23:38:05 +0300

    .

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+        int price = 7000;
+
+        int rub = 20; // Бонусные рубли
+
+        int miles = price / rub;
+
+
+        return miles;
+    }
+}
