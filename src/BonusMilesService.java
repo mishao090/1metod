41438a127c9f06f07424894e42f77c43176e90f6
mishao090 <michailus321@gmail.com>
2022-06-25 23:32:40 +0300

    Исправление ошибки

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,9 @@
 public class BonusMilesService {
     public int calculate(int cost) {
 
-        int price = 7000;
-
         int rub = 20; // Бонусные рубли
 
-        int miles = price / rub;
-
+        int miles = cost / rub;
 
         return miles;
     }
